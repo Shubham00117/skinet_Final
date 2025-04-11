4aefbbfb1a18d77f2c3ce33d9f8871a1e7341934
Shubham00117 <shubhamshinde5150@gmail.com>
2025-04-11 12:51:16 +0530

    Final_Commit

---
+++ client/src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:5001/api/',
+  hubUrl: 'https://localhost:5001/hub/notifications',
+  stripePublicKey:
+    'pk_test_51RAQI6BceHsZGzIMdLNT217jmUJg4svrWPwoQGuTybtgvZI5hb3wpUymXcvcNo7b7UwOZYOUfCMS3XBVXZOntPJp003ADIn7Cr',
+};
